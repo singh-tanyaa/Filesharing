a4336a0633d843f026dec0bbe62fff2b7dbac351
rajPadval <97424854+rajPadval@users.noreply.github.com>
2023-12-30 15:55:39 +0530

    Fixed issues

--- server/index.js
+++ server/index.js
@@ -3,9 +3,7 @@ import cors from "cors";
 import router from "./routes/routes.js";
 import DBConnection from "./database/db.js";
 import dotenv from "dotenv";
-import path from 'path';
-
-
+import path from "path";
 
 dotenv.config();
 
@@ -16,10 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", router);
 //static files
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(path.dirname("./client/build"))));
 
-app.get('*', fuction(req,res){
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 const PORT = process.env.PORT || 8000;
 
